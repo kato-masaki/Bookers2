ba19580fc544479c577774b9817b0fae0416fd48
kato-masaki <0913ritz@gmail.com>
2021-02-22 08:54:15 +0000

    [Finish] STEP3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
+  
   get 'users/index'
   get 'users/show'
   get 'users/edit'
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 end
